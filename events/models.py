13e896aea21af78499e5f781bdc122055551cb2c
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-09-14 11:46:45 +0100

    done

--- events/models.py
+++ events/models.py
@@ -6,9 +6,9 @@ class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=100)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Contact Phone', max_length=100)
-    web = models.URLField('Website Address')
-    email = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=100, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email = models.EmailField('Email Address', blank=True)
 
     def __str__(self):
         return self.name.title()
@@ -27,8 +27,7 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=200)
     event_date = models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True, null=True)
-    # venue = models.CharField(max_length=200)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, related_name='attendees', blank=True)
 
