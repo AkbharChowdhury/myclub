649573b79dc882ea7cb16ea8d248f664813ae538
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-09-15 16:02:04 +0100

    s

--- events/urls.py
+++ events/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 # . means current directory
 from . import views
 
+from .views import VenueCreateView
+
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('<int:year>/<str:month>', views.home, name='home'),
     path('events/', views.all_events, name='list-events'),
+    # path('add_venue', views.add_venue, name='add_venue'),
+    path('add_venue', VenueCreateView.as_view(), name='add_venue'),
 
 ]
--- events/views.py
+++ events/views.py
@@ -4,14 +4,19 @@ from django.shortcuts import render
 from calendar import HTMLCalendar
 import calendar
 from datetime import datetime
-from .models import Event
+
+from django.views.generic import CreateView
+
+from .models import Event, Venue
+from .forms import VenueForm
+from django.http import HttpResponseRedirect
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime("%B")):
     name = 'john'.title()
     month_num = list(calendar.month_name).index(month)
     month_num = int(month_num)
-    print(month_num)
     cal = HTMLCalendar().formatmonth(year, month_num)
     now = datetime.now()
     current_year = now.year
@@ -28,3 +33,28 @@ def home(request, year=datetime.now().year, month=datetime.now().strftime("%B"))
 def all_events(request):
     event_list = Event.objects.all()
     return render(request, 'event_list.html', {'event_list': event_list})
+
+
+# def add_venue(request):
+#     is_submitted = False
+#     if request.method == 'POST':
+#         form = VenueForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#
+#             return HttpResponseRedirect('/add_venue?is_submitted=True')
+#         else:
+#             is_submitted = True
+#
+#     form = VenueForm
+#     return render(request, 'add_venue.html', {'form': form, 'is_submitted': is_submitted})
+
+
+class VenueCreateView(SuccessMessageMixin, CreateView):
+    model = Venue
+    context_object_name = 'venue'
+    template_name = 'add_venue.html'
+    fields = '__all__'
+    success_message = f'venue created'.title()
+    success_url = 'add_venue'
+
