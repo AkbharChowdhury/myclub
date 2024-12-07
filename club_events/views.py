c533c557e2a4cdd3f824c31b15f3a141a9c54ba8
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-07 21:51:32 +0000

    x

--- club_events/views.py
+++ club_events/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from calendar import month_name, HTMLCalendar
+import datetime
+import calendar
+import datetime
+
 
 # Create your views here.
-def home(request):
-    return render(request,'home.html',{})
+# def home(request):
+#     return render(request,'home.html',{})
+
+def home(request, year: int = datetime.date.year, month: str = "Jan"):
+    month_num = list(calendar.month_name).index(month.title())
+    return render(request, 'home.html', {
+        'year': year,
+        'month': month,
+        'my_calendar': HTMLCalendar().formatmonth(year, month_num),
+    })
