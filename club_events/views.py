34935aa8a2e9930015531ee5f9371580df4c6341
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-09 15:07:59 +0000

    x

--- club_events/views.py
+++ club_events/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from calendar import HTMLCalendar
 import calendar
 from calendar_utils import CalendarUtils
-
+from .models import Event
 
 def home(request, year: int = CalendarUtils.current_year(), month: str = CalendarUtils.current_month()):
     month_num = list(calendar.month_name).index(month.title())
@@ -10,4 +10,11 @@ def home(request, year: int = CalendarUtils.current_year(), month: str = Calenda
         'year': year,
         'month': month,
         'my_calendar': HTMLCalendar().formatmonth(year, month_num),
+    })
+
+
+def all_events(request):
+    events = Event.objects.all()
+    return render(request, 'event_list.html', {
+        'events': events,
     })
