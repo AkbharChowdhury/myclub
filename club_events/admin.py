77771bed84adb1b9e4cddc3dd04e187e1c42f107
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-09 17:42:56 +0000

    x

--- club_events/admin.py
+++ club_events/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Venue, Event, MyClubUser
 
-admin.site.register(Venue)
-admin.site.register(Event)
+# admin.site.register(Venue)
+# admin.site.register(Event)
 admin.site.register(MyClubUser)
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    search_fields = ('name', 'address')
+    ordering = ['name']
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date_time', 'description', 'manager')
+    list_display = ('name', 'event_date_time', 'venue')
+    list_filter = ('event_date_time', 'venue')
+    ordering = ['event_date_time']
--- club_events/models.py
+++ club_events/models.py
@@ -6,9 +6,9 @@ class Venue(models.Model):
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=300)
     zip_code = models.CharField(max_length=15)
-    phone = models.CharField(max_length=20)
-    web = models.URLField()
-    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    web = models.URLField(blank=True, null=True)
+    email = models.EmailField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -18,7 +18,7 @@ class Event(models.Model):
     name = models.CharField(max_length=120)
     event_date_time = models.DateTimeField()
     venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True, null=True)
-    manager = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(User, related_name='attendees', blank=True)
 
