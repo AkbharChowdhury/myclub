6c8c2834fbe15ad303ed31124ac4f83afdc21475
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-13 19:00:02 +0000

    x

--- club_events/static/js/event_date_time.js
+++ club_events/static/js/event_date_time.js
@@ -1,12 +1,10 @@
 const event_date_time = '#id_event_date_time';
 const date_time = document.querySelector(event_date_time).value;
 flatpickr(event_date_time, {
-
     enableTime: true,
     altInput: true,
     dateFormat: "Y-m-d H:i",
     altFormat: 'l J F, Y h:i K',
     minDate: 'today',
     defaultDate: date_time !== '' ? date_time : new Date(),
-
 });
