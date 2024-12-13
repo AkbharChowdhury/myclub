b824a1e0c2541d8d38ac0324795cb0ae4c731040
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-13 18:50:56 +0000

    x

--- club_events/forms.py
+++ club_events/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 from django.contrib.auth.models import User
-from django_flatpickr.widgets import DateTimePickerInput
-
 from .models import Venue, Event
 
 
@@ -12,13 +10,11 @@ class VenueForm(forms.ModelForm):
 
 
 class EventForm(forms.ModelForm):
-    # attendees = forms.MultiValueField(queryset=User.objects.filter(is_staff=True))
-    # attendees = forms.MultiValueField(queryset=User.objects.filter(is_staff=True), fields=)
-    attendees = forms.ModelMultipleChoiceField(queryset=User.objects.filter(is_active=True), widget=forms.CheckboxSelectMultiple())
+    attendees = forms.ModelMultipleChoiceField(queryset=User.objects.filter(is_active=True),
+                                               widget=forms.CheckboxSelectMultiple())
     manager = forms.ModelChoiceField(queryset=User.objects.filter(is_staff=True))
     event_date_time = forms.DateTimeField()
 
-    # https://stackoverflow.com/questions/74043096/django-crispy-form-filter-by-is-staff
     class Meta:
         model = Event
         fields = '__all__'
---
+++ club_events/static/js/event_date_time.js
@@ -0,0 +1,12 @@
+const event_date_time = '#id_event_date_time';
+const date_time = document.querySelector(event_date_time).value;
+flatpickr(event_date_time, {
+
+    enableTime: true,
+    altInput: true,
+    dateFormat: "Y-m-d H:i",
+    altFormat: 'l J F, Y H:i',
+    minDate: 'today',
+    defaultDate: date_time !== '' ? date_time : new Date(),
+
+});
