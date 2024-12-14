3762cbe223c5a9b50ba96675f24beaca62c5ae4a
Akbhar <112480476+AkbharChowdhury@users.noreply.github.com>
2024-12-14 12:34:51 +0000

    x

--- club_events/admin.py
+++ club_events/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Venue, Event, MyClubUser
+from .models import Venue, Event
 
-admin.site.register(MyClubUser)
 
 
 @admin.register(Venue)
--- club_events/models.py
+++ club_events/models.py
@@ -26,12 +26,3 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class MyClubUser(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField()
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
